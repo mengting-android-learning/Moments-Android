4524fcb7e4ba33f2fc95d5e8e09c57e10a535cb6
Rachel-oc <mengting.xie@thoughtworks.com>
2023-03-16 15:31:06 +0800

    fix: hide navigation bar and status bar

--- app/src/main/java/com/example/momentsrecyclerview/ui/MomentsActivity.kt
+++ app/src/main/java/com/example/momentsrecyclerview/ui/MomentsActivity.kt
@@ -1,12 +1,16 @@
 package com.example.momentsrecyclerview.ui
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.momentsrecyclerview.R
 
 class MomentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_moments)
     }
 }
