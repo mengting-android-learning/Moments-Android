e7b84548bacb34625c684c1d0913e459be54ce1d
Rachel-oc <mengting.xie@thoughtworks.com>
2023-03-29 10:52:31 +0800

    feat:add loading img for compose moments

--- app/src/main/java/com/example/momentsrecyclerview/ui/compose/MomentsDescription.kt
+++ app/src/main/java/com/example/momentsrecyclerview/ui/compose/MomentsDescription.kt
@@ -1,5 +1,6 @@
 package com.example.momentsrecyclerview.ui.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,14 +17,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -36,13 +44,44 @@ import com.example.momentsrecyclerview.domain.Tweet
 import com.example.momentsrecyclerview.domain.TweetComment
 import com.example.momentsrecyclerview.domain.UserInfo
 import com.example.momentsrecyclerview.ui.MomentsViewModel
+import com.example.momentsrecyclerview.ui.STATUS
 
 @Composable
 fun MomentsDescription(momentsViewModel: MomentsViewModel) {
-    val userInfo by momentsViewModel.userInfo.observeAsState()
-    val tweets by momentsViewModel.tweetsList.observeAsState()
-    if (userInfo != null && tweets != null) {
-        Moments(userInfo = userInfo!!, tweets = tweets!!)
+    var showLandingScreen by remember { mutableStateOf(true) }
+    if (showLandingScreen) {
+        LandingScreen(
+            onTimeout = { showLandingScreen = false },
+            momentsViewModel = momentsViewModel
+        )
+    } else {
+        val userInfo by momentsViewModel.userInfo.observeAsState()
+        val tweets by momentsViewModel.tweetsList.observeAsState()
+        if (userInfo != null && tweets != null) {
+            Moments(userInfo = userInfo!!, tweets = tweets!!)
+        }
+    }
+}
+
+@Composable
+fun LandingScreen(
+    modifier: Modifier = Modifier,
+    onTimeout: () -> Unit,
+    momentsViewModel: MomentsViewModel
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+        val status by momentsViewModel.status.observeAsState()
+        LaunchedEffect(status) {
+            if (status == STATUS.DONE) {
+                currentOnTimeout()
+            }
+        }
+        Image(
+            painterResource(id = R.drawable.loading_img),
+            contentDescription = "loading",
+            modifier = modifier.fillMaxSize()
+        )
     }
 }
 
