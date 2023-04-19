841c0249971b71a69444edfbe047665ffe935c5a
Rachel-oc <mengting.xie@thoughtworks.com>
2023-04-19 11:41:20 +0800

    refactor:extract methods

--- app/src/main/java/com/example/momentsrecyclerview/data/TweetsRepository.kt
+++ app/src/main/java/com/example/momentsrecyclerview/data/TweetsRepository.kt
@@ -2,6 +2,7 @@ package com.example.momentsrecyclerview.data
 
 import com.example.momentsrecyclerview.data.source.local.MomentsDatabaseDao
 import com.example.momentsrecyclerview.data.source.network.NetworkTweetsService
+import com.example.momentsrecyclerview.domain.Sender
 import com.example.momentsrecyclerview.domain.Tweet
 import com.example.momentsrecyclerview.domain.mapper.local.asDomainTweet
 import com.example.momentsrecyclerview.domain.mapper.local.toLocalImage
@@ -26,28 +27,54 @@ class LocalTweetsRepository(private val dataSource: MomentsDatabaseDao) : Tweets
 
     suspend fun saveTweets(tweets: List<Tweet>) {
         val localUserInfo = dataSource.getUserInfo()
+        clearAllData()
+        if (localUserInfo != null) dataSource.insertUser(localUserInfo)
+        for (tweet in tweets) {
+            val senderId = insertUser(tweet.sender)
+            val tweetId = insertTweet(tweet, senderId)
+            insertComments(tweet, tweetId)
+            insertImages(tweet, tweetId)
+        }
+    }
+
+    private suspend fun insertComments(
+        tweet: Tweet,
+        tweetId: Long
+    ) {
+        if (tweet.comments?.isNotEmpty() == true) {
+            for (comment in tweet.comments) {
+                val commentSenderId = insertUser(comment.sender)
+                dataSource.insertTweetComment(
+                    comment.toLocalTweetComment(
+                        commentSenderId,
+                        tweetId
+                    )
+                )
+            }
+        }
+    }
+
+    private suspend fun insertImages(
+        tweet: Tweet,
+        tweetId: Long
+    ) {
+        if (tweet.images?.isNotEmpty() == true) {
+            dataSource.insertImage(tweet.images.map { it.toLocalImage(tweetId) })
+        }
+    }
+
+    private suspend fun insertTweet(
+        tweet: Tweet,
+        senderId: Long
+    ) = dataSource.insertTweet(tweet.toLocalTweet(senderId))
+
+    private suspend fun insertUser(sender: Sender) =
+        dataSource.insertUser(sender.toLocalUser())
+
+    private suspend fun clearAllData() {
         dataSource.deleteTweets()
         dataSource.deleteComments()
         dataSource.deleteImages()
         dataSource.deleteUsers()
-        if (localUserInfo != null) dataSource.insertUser(localUserInfo)
-        for (tweet in tweets) {
-            val senderId = dataSource.insertUser(tweet.sender.toLocalUser())
-            val tweetId = dataSource.insertTweet(tweet.toLocalTweet(senderId))
-            if (tweet.comments?.isNotEmpty() == true) {
-                for (comment in tweet.comments) {
-                    val commentSenderId = dataSource.insertUser(comment.sender.toLocalUser())
-                    dataSource.insertTweetComment(
-                        comment.toLocalTweetComment(
-                            commentSenderId,
-                            tweetId
-                        )
-                    )
-                }
-            }
-            if (tweet.images?.isNotEmpty() == true) {
-                dataSource.insertImage(tweet.images.map { it.toLocalImage(tweetId) })
-            }
-        }
     }
 }
